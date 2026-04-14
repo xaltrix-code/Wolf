ee542fe3c1ebabf251cc4040926770f982e00828
Aerolynx <lonelyhandsomedude@hotmail.com>
2026-04-14 18:33:27 +0530

    Implemented the clientReady event

--- src/Wolf.ts
+++ src/Wolf.ts
@@ -1,4 +1,7 @@
 import { Client } from "discord.js";
+import { join, dirname } from "path";
+import { fileURLToPath, pathToFileURL } from "url";
+import { readdirSync } from "fs";
 
 class Wolf extends Client {
     constructor() {
@@ -9,8 +12,33 @@ class Wolf extends Client {
 
     async launch(token: string | undefined) {
         await this.login(token);
+        await this.loadEvents();
 
         console.log(`Successfully logged in as ${this.user?.tag} (${this.user?.id})`);
+    }
+
+    async loadEvents() {
+        const __filname = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filname);
+
+        const eventsPath = join(__dirname, "events");
+        const eventFiles = readdirSync(eventsPath)
+            .filter(file => file.endsWith(".js"));
+
+            let count: number = 0;
+
+            for (const file of eventFiles) {
+                const filePath = join(eventsPath, file);
+
+                const { default: events } = await import(pathToFileURL(filePath).href);
+                const eventName = file.split(".")[0];
+
+                this.on(eventName!, events.bind(null, this));
+
+                count++;
+            }
+
+            console.log(`Loaded ${count} events`);
     }
 }
 
